e3330101b51fb37fc12a4cf17b544e4619ca3e7a
AnnekaKing <annekak123@gmail.com>
2023-01-28 21:45:13 -0600

    Created website

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+const pagesRouter = require('./controller/pages')
+const app = express()
+port = process.env.PORT || 80
+
+app.set('view engine', 'ejs')
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', pagesRouter)
+
+app.listen(port, () => {
+    console.log('server is running....')
+})
